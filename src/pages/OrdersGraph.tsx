a9ecaae04a4936ce138375eef26f1251ead26f73
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-09-30 14:14:13 +0200

    refactor: Updating AppRoutes file and add new route

---
+++ src/pages/OrdersGraph.tsx
@@ -0,0 +1,7 @@
+export function OrdersGraph() {
+  return (
+    <>
+      <h1>Welcome to OrdersGraph!!</h1>
+    </>
+  )
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import { Home } from "@/pages/Home";
 import { Order } from "@/pages/Order";
+import { OrdersGraph } from "@/pages/OrdersGraph";
 import { Route, Routes } from "react-router-dom";
 
 export function AppRoutes() {
@@ -7,6 +8,7 @@ export function AppRoutes() {
     <Routes>
       <Route element={<Home />} index />
       <Route element={<Order />} path="/order/:id" />
+      <Route element={<OrdersGraph />} path="/orders-graph" />
     </Routes>
   )
 }
