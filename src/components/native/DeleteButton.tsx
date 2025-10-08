91740a36cf425152c53b3a8e310a780674bb770f
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-08 09:54:00 +0200

    feat: Implement state to manage if modal is open and function to call onDelete pass on params

--- src/components/native/DeleteButton.tsx
+++ src/components/native/DeleteButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { 
   AlertDialog, 
   AlertDialogCancel, 
@@ -13,11 +14,19 @@ import { Button } from "../ui/button"
 
 interface DeleteButtonParams {
   children: React.ReactNode
+  onDelete: () => Promise<void>
 }
 
-export function DeleteButton({ children }: DeleteButtonParams) {
+export function DeleteButton({ children, onDelete }: DeleteButtonParams) {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  async function handleClickToDelete() {
+    await onDelete()
+    setIsOpen(false)
+  }
+  
   return (
-    <AlertDialog>
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
       <AlertDialogTrigger asChild>
         {children}
       </AlertDialogTrigger>
@@ -30,7 +39,13 @@ export function DeleteButton({ children }: DeleteButtonParams) {
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel className="cursor-pointer">Cancelar</AlertDialogCancel>
-          <Button variant="destructive" className="cursor-pointer">Continue</Button>
+          <Button 
+            variant="destructive" 
+            className="cursor-pointer"
+            onClick={handleClickToDelete}
+          >
+            Continue
+          </Button>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
