a4a5cd822cb867eda2fbb06b1f8580329bf9f379
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-08 10:09:37 +0200

    feat: Creating Form page and function to crete an new order based on title and description states. Move it into userPages folder

---
+++ src/pages/userPages/Form.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { api } from "@/lib/axios";
+import { useState } from "react";
+
+export function Form() {
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+
+
+  async function createOrder() {
+    try {
+      const response = await api.post('/order', {
+        title: title,
+        description: description
+      })
+
+      alert(response.data.message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Card className="w-1/2 h-10/12 p-8 overflow-hidden">
+        <CardHeader>
+          <CardTitle className="text-2xl">Formulário de pedido</CardTitle>
+          <CardDescription className="text-md">Faça seu pedido agora mesmo</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="title">Título</label>
+              <Input 
+                type="text"
+                id="title"
+                placeholder="Digite o título do pedido..."
+                onChange={(event) => setTitle(event.target.value)}
+                value={title}
+              />
+            </div>
+            
+            <div className="flex flex-col gap-2">
+              <label htmlFor="description">Descrição</label>
+              <Textarea 
+                id="description"
+                className="h-32"
+                placeholder="Digite a descrição do pedido..."
+                onChange={(event) => setDescription(event.target.value)}
+                value={description}
+              />
+            </div>
+
+            <div>
+              <Button 
+                type="submit"
+                variant="success"
+                onClick={() => createOrder()}
+              >
+                Enviar pedido
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
