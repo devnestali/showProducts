d32edbdffc15c4aa0db43db49f5cb40ec8e039ff
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-08 09:48:53 +0200

    feat: Creating axios configuration

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,21 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://localhost:3333',
+  withCredentials: true
 })
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('@showProducts@user:token')
-
-    if(token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
 
