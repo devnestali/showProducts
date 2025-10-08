6b4e4c0dbd7ac6790eaf12b59101510a966029f1
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-08 10:05:37 +0200

    feat: Creating function to initialize session based on values of email and password states

---
+++ src/pages/signPages/Login.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useAuth } from "@/contexts/authContext";
+
+export function Login() {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  
+  const navigate = useNavigate()
+  const { signIn } = useAuth()
+  
+  function handleNavigationToRegister() {
+    navigate('/register')
+  }
+
+  function onChangeEmailInput(value: string) {
+    setEmail(value)
+  }
+
+  function onChangePasswordInput(value: string) {
+    setPassword(value)
+  }
+
+  async function initializeSession() {
+    try {
+      if(email && password) {
+        signIn(email, password)
+        
+        navigate('/')
+      }
+    } catch (error) {
+      alert('Houve um erro na aplicacao')
+    }
+    
+    
+  }
+  
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Card className="h-[430px] w-[520px] p-8">
+        <CardHeader>
+          <CardTitle className="text-lg">Fazer Login</CardTitle>
+          <CardDescription>Entre na sua conta para acessar seu painel.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-3">
+              <label htmlFor="email" className="text-sm">E-mail</label>
+              <Input 
+                type="email"
+                id="email" 
+                className="w-full" 
+                placeholder="Digite o seu e-mail..."
+                onChange={(event) => onChangeEmailInput(event.target.value)}
+                value={email}
+              />
+            </div>
+            
+            <div className="flex flex-col gap-3">
+              <label htmlFor="password" className="text-sm">Senha</label>
+              <Input 
+                type="password"
+                id="password" 
+                className="w-full" 
+                placeholder="Digite o sua senha..."
+                onChange={(event) => onChangePasswordInput(event.target.value)}
+                value={password}
+              />
+            </div>
+
+            <div className="flex flex-col gap-3 mt-8">
+              <Button 
+                variant="default"
+                onClick={() => initializeSession()}
+                >
+                  Entrar
+                  </Button>
+              <Button 
+                variant="link"
+                onClick={() => handleNavigationToRegister()}
+              >
+                Registrar-se
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
