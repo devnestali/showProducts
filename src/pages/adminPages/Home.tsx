a47f3032a5d1a51f4b4e824e3bdb0417f11b28b7
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-08 10:00:03 +0200

    feat: Creating function to fetch for orders and state to store it. Move this file to adminPages folder

---
+++ src/pages/adminPages/Home.tsx
@@ -0,0 +1,45 @@
+import { Header } from "@/components/native/Header"
+import { Product } from "@/components/native/Product"
+import { api } from "@/lib/axios"
+
+import { useEffect, useState } from "react"
+
+export interface Order {
+  id: number
+  title: string
+  description: string
+  orderDate: string
+  userId: number
+}
+
+export function Home() {
+  const [orders, setOrders] = useState<Order[]>([])
+  
+  useEffect(() => {
+    async function fetchForOrders() {
+      const { data } = await api.get('/order')
+
+      setOrders(data)
+    }
+
+    fetchForOrders()
+  }, [])
+  return (
+    <div className="w-screen">
+      <Header />
+
+      <div className="flex flex-col gap-4 px-16 py-8">
+        {
+          orders.map((order, _) => {
+            return (
+              <Product 
+                key={order.id} 
+                data={order}
+              />
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
